2df921febb7766c957151cadfd74d0b889350153
baizel <baizelkv@gmail.com>
2022-03-03 22:50:59 +0530

    veriion 0.0.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ USER_NAME = "devbk007"
 
 setuptools.setup(
     name=f"{PROJECT_NAME}-{USER_NAME}",
-    version="0.0.2",
+    version="0.0.3",
     author=USER_NAME,
     author_email="baizelkv@gmail.com",
     description="A small package for perceptron",
